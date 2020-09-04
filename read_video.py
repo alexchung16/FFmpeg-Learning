cf6ec796336ccf98eac60eb1dfb00b80ddde194a
alexchung <yonganzhong@outlook.com>
2020-09-04 17:16:58 +0800

    update read_video

--- read_video.py
+++ read_video.py
@@ -24,65 +24,14 @@ input_video = './data/rafting.avi'
 output_video = './outputs/flip.mp4'
 output_filename = './outputs/thumbnail.jpeg'
 
-if __name__ == "__main__":
-    #
-    # flip video
-    # stream = ffmpeg.input(input_video)
-    # stream = ffmpeg.hflip(stream)
-    # stream = ffmpeg.output(stream, output_video)
-    # stream = ffmpeg.overwrite_output(stream)
-    # ffmpeg.run(stream, capture_stdout=True, capture_stderr=True)
-    # ffmpeg.run(stream)
+def read_show_video(video_path):
+    """
 
-
-
-    # get video info
-    # probe = ffmpeg.probe(input_video)
-    # print(probe["streams"])
-    # print(probe['format'])
-    #
-    # # video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
-    # # width = int(video_stream['width'])
-    # # height = int(video_stream['height'])
-    #
-    # for info in probe["streams"]:
-    #     for k, v in info.items():
-    #         print(k, ':', v)
-    #
-    # for k, v in probe['format'].items():
-    #     print(k, ':', v)
-
-
-    # general thumbnail for video
-    # try:
-    #     stream = ffmpeg.input(input_video, ss=0.1)
-    #     stream = ffmpeg.filter(stream, 'scale', 120, -1)
-    #     stream = ffmpeg.output(stream, output_filename, vframes=1)
-    #     stream = ffmpeg.overwrite_output(stream)
-    #     ffmpeg.run(stream, capture_stdout=True, capture_stderr=True)
-    # except ffmpeg.Error as e:
-    #     print(e.stderr.decode(), file=sys.stderr)
-    #     raise e
-
-    # def generate_thumbnail(in_filename, out_filename, time, width):
-    #     try:
-    #         (
-    #             ffmpeg
-    #                 .input(in_filename, ss=time)
-    #                 .filter('scale', width, -1)
-    #                 .output(out_filename, vframes=1)
-    #                 .overwrite_output()
-    #                 .run(capture_stdout=True, capture_stderr=True)
-    #         )
-    #     except ffmpeg.Error as e:
-    #         print(e.stderr.decode())
-    #
-    # generate_thumbnail(in_filename=input_video, out_filename=output_filename, time=0.1, width=120)
-
-    # convert video to numpy array
-
+    :param video_path:
+    :return:
+    """
     try:
-        probe = ffmpeg.probe(input_video)
+        probe = ffmpeg.probe(video_path)
         # get video info except audio
         video_info = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
         height = int(video_info['height'])
@@ -113,6 +62,64 @@ if __name__ == "__main__":
     except ffmpeg.Error as e:
         print(e.stderr.decode(), file=sys.stderr)
         raise e
+
+
+def generate_thumbnail(in_filename, out_filename, time, width):
+    try:
+        (
+            ffmpeg
+                .input(in_filename, ss=time)
+                .filter('scale', width, -1)
+                .output(out_filename, vframes=1)
+                .overwrite_output()
+                .run(capture_stdout=True, capture_stderr=True)
+        )
+    except ffmpeg.Error as e:
+        print(e.stderr.decode())
+
+if __name__ == "__main__":
+    #
+    # flip video
+    # stream = ffmpeg.input(input_video)
+    # stream = ffmpeg.hflip(stream)
+    # stream = ffmpeg.output(stream, output_video)
+    # stream = ffmpeg.overwrite_output(stream)
+    # ffmpeg.run(stream, capture_stdout=True, capture_stderr=True)
+    # ffmpeg.run(stream)
+    # get video info
+    input_video = './data/rafting.avi'
+
+    probe = ffmpeg.probe(input_video)
+
+    video_stream_info = probe["streams"][0]
+    audio_stream_info = probe["streams"][1]
+    format_info = probe["format"]
+    # show codec info
+    print("codec_type: {0} | codec_name: {1}| codec_long_name: {2}".format(video_stream_info["codec_type"],
+                                                                         video_stream_info["codec_name"],
+                                                                         video_stream_info["codec_long_name"]))
+    print("codec_type: {0} | codec_name: {1}| codec_long_name: {2}".format(audio_stream_info["codec_type"],
+                                                                           audio_stream_info["codec_name"],
+                                                                           audio_stream_info["codec_long_name"]))
+    # show container(encapsulation) format
+    print("filename:{0} | format_name: {1} | format_long_name: {2}".format(format_info["filename"],
+                                                                           format_info["format_name"],
+                                                                           format_info["format_long_name"]))
+    # codec_type: video | codec_name: mpeg4| codec_long_name: MPEG-4 part 2
+    # filename:./data/rafting.avi | format_name: avi | format_long_name: AVI (Audio Video Interleaved)
+
+    # for info in probe["streams"]:
+    #     for k, v in info.items():
+    #         print(k, ':', v)
+    #
+    # print('-' * 40)
+    # for k, v in probe['format'].items():
+    #     print(k, ':', v)
+
+    # read_show_video(input_video)
+    # generate_thumbnail(in_filename=input_video, out_filename=output_filename, time=0.1, width=120)
+
+
 
 
 
